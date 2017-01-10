ec54d4e982eba9af7ec96def503791e5db7e7bc3
廖师兄 <null@163.com>
2017-01-11 00:33:17 +0800

    整理好代码

---
+++ src/main/java/com/imooc/controller/GirlController.java
@@ -0,0 +1,84 @@
+package com.imooc.controller;
+
+import com.imooc.domain.Girl;
+import com.imooc.repository.GirlRepository;
+import com.imooc.service.GirlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by 廖师兄
+ * 2016-11-03 23:15
+ */
+@RestController
+public class GirlController {
+
+    @Autowired
+    private GirlRepository girlRepository;
+
+    @Autowired
+    private GirlService girlService;
+
+    /**
+     * 查询所有女生列表
+     * @return
+     */
+    @GetMapping(value = "/girls")
+    public List<Girl> girlList() {
+        return girlRepository.findAll();
+    }
+
+    /**
+     * 添加一个女生
+     * @param cupSize
+     * @param age
+     * @return
+     */
+    @PostMapping(value = "/girls")
+    public Girl girlAdd(@RequestParam("cupSize") String cupSize,
+                          @RequestParam("age") Integer age) {
+        Girl girl = new Girl();
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+
+        return girlRepository.save(girl);
+    }
+
+    //查询一个女生
+    @GetMapping(value = "/girls/{id}")
+    public Girl girlFindOne(@PathVariable("id") Integer id) {
+        return girlRepository.findOne(id);
+    }
+
+    //更新
+    @PutMapping(value = "/girls/{id}")
+    public Girl girlUpdate(@PathVariable("id") Integer id,
+                           @RequestParam("cupSize") String cupSize,
+                           @RequestParam("age") Integer age) {
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+
+        return girlRepository.save(girl);
+    }
+
+    //删除
+    @DeleteMapping(value = "/girls/{id}")
+    public void girlDelete(@PathVariable("id") Integer id) {
+        girlRepository.delete(id);
+    }
+
+    //通过年龄查询女生列表
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> girlListByAge(@PathVariable("age") Integer age) {
+        return girlRepository.findByAge(age);
+    }
+
+    @PostMapping(value = "/girls/two")
+    public void girlTwo() {
+        girlService.insertTwo();
+    }
+}
