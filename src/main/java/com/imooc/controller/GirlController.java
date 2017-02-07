6c3167cecb5e6e4005991e25d21ca196aec0dd50
廖师兄 <null@163.com>
2017-02-07 22:09:30 +0800

    统一异常处理

--- src/main/java/com/imooc/controller/GirlController.java
+++ src/main/java/com/imooc/controller/GirlController.java
@@ -1,8 +1,10 @@
 package com.imooc.controller;
 
 import com.imooc.domain.Girl;
+import com.imooc.domain.Result;
 import com.imooc.repository.GirlRepository;
 import com.imooc.service.GirlService;
+import com.imooc.utils.ResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,16 +45,15 @@ public class GirlController {
      * @return
      */
     @PostMapping(value = "/girls")
-    public Girl girlAdd(@Valid Girl girl, BindingResult bindingResult) {
+    public Result<Girl> girlAdd(@Valid Girl girl, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            System.out.println(bindingResult.getFieldError().getDefaultMessage());
-            return null;
+            return ResultUtil.error(1, bindingResult.getFieldError().getDefaultMessage());
         }
 
         girl.setCupSize(girl.getCupSize());
         girl.setAge(girl.getAge());
 
-        return girlRepository.save(girl);
+        return ResultUtil.success(girlRepository.save(girl));
     }
 
     //查询一个女生
@@ -89,5 +90,10 @@ public class GirlController {
     @PostMapping(value = "/girls/two")
     public void girlTwo() {
         girlService.insertTwo();
+    }
+
+    @GetMapping(value = "girls/getAge/{id}")
+    public void getAge(@PathVariable("id") Integer id) throws Exception{
+        girlService.getAge(id);
     }
 }
--- src/main/java/com/imooc/service/GirlService.java
+++ src/main/java/com/imooc/service/GirlService.java
@@ -1,6 +1,8 @@
 package com.imooc.service;
 
 import com.imooc.domain.Girl;
+import com.imooc.enums.ResultEnum;
+import com.imooc.exception.GirlException;
 import com.imooc.repository.GirlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +30,20 @@ public class GirlService {
         girlB.setCupSize("BBBB");
         girlB.setAge(19);
         girlRepository.save(girlB);
+    }
+
+    public void getAge(Integer id) throws Exception{
+        Girl girl = girlRepository.findOne(id);
+        Integer age = girl.getAge();
+        if (age < 10) {
+            //返回"你还在上小学吧" code=100
+            throw new GirlException(ResultEnum.PRIMARY_SCHOOL);
+        }else if (age > 10 && age < 16) {
+            //返回"你可能在上初中" code=101
+            throw new GirlException(ResultEnum.MIDDLE_SCHOOL);
+        }
+
+        //如果>16岁,加钱
+        //...
     }
 }
